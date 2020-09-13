22cf7dcfd6305fba39a83449fe26b56c1e500dcc
YusefB <67576331+YusefB@users.noreply.github.com>
2020-09-13 11:34:51 -0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,9 @@
+ #Yusef Bayyat
+ #7/6/20 
+#The purpose of the Code is to give some information about me 
+def main(): 
+	print("My name is Yusef Bayyat and im 13 as of today") 
+print("My birthday is August 16, 2006") 
+print("My favorite sports are Basketball and Soccer") 
+print("A fun fact about me is thaa i have met numerous famous basketball") print("I already know how to code some basic lines in HTML5 and I already know some code in python") 
+main()
